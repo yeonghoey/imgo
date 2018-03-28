047c39ffa9b36e494fa20476a14bd7c688a70470
Yeongho Kim <yeonghoey@gmail.com>
2018-03-28 11:58:03 +0900

    Implement basics

--- imgo/__main__.py
+++ imgo/__main__.py
@@ -1,9 +1,19 @@
 import click
+from PIL import ImageGrab
+import pyperclip
+import pytesseract
 
 
 @click.command()
-def cli():
-    pass
+@click.argument('lang')
+def cli(lang):
+    img = ImageGrab.grabclipboard()
+    if img is None:
+        raise click.UsageError('Clipboard does not contain image')
+    else:
+        s = pytesseract.image_to_string(img, lang=lang)
+        pyperclip.copy(s)
+        click.echo(s)
 
 
 if __name__ == '__main__':
